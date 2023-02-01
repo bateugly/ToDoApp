479e41e1a1503d5e196bdeb9b69a4530a9d86c22
BATE <bate.ugly@gmail.com>
2023-02-01 15:39:59 +0700

    first

--- firebase.js
+++ firebase.js
@@ -3,13 +3,21 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDaUitBcy17bNrIUZ1l5bI3yBkyAF_lSps",
-  authDomain: "todoapp-cc8ba.firebaseapp.com",
-  projectId: "todoapp-cc8ba",
-  storageBucket: "todoapp-cc8ba.appspot.com",
-  messagingSenderId: "327285561962",
-  appId: "1:327285561962:web:afdbd615b7eb492b2f0813",
-  measurementId: "G-YEJX4F991Q"
+  // apiKey: "AIzaSyDaUitBcy17bNrIUZ1l5bI3yBkyAF_lSps",
+  // authDomain: "todoapp-cc8ba.firebaseapp.com",
+  // projectId: "todoapp-cc8ba",
+  // storageBucket: "todoapp-cc8ba.appspot.com",
+  // messagingSenderId: "327285561962",
+  // appId: "1:327285561962:web:afdbd615b7eb492b2f0813",
+  // measurementId: "G-YEJX4F991Q"
+  apiKey: "AIzaSyAphI8yenvlEMXs3onn7nmrY1rtqIl95eY",
+  authDomain: "bate-e7b13.firebaseapp.com",
+  databaseURL: "https://bate-e7b13-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "bate-e7b13",
+  storageBucket: "bate-e7b13.appspot.com",
+  messagingSenderId: "472837828353",
+  appId: "1:472837828353:web:a3604fb6a58ed891177500",
+  measurementId: "G-CYG58XMGM9"
 };
 
 const app = initializeApp(firebaseConfig);
